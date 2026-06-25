f0b2408d60ed6a7ea357785d8484fa2d7e4cd605
kuntrapakampranavi <kuntrapakampranavi@gmail.com>
2026-06-25 10:42:38 +0800

    Initial Commit

---
+++ solos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  solos
+//
+//  Created by Pranavi Kuntrapakam on 25/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ solos/solosApp.swift
@@ -0,0 +1,17 @@
+//
+//  solosApp.swift
+//  solos
+//
+//  Created by Pranavi Kuntrapakam on 25/06/26.
+//
+
+import SwiftUI
+
+@main
+struct solosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
